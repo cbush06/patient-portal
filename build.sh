e6fb63c95302404474961e8f45576da76e9b5d7c
Clinton Bush <clinton.bush@outlook.com>
2024-01-04 14:12:48 -0500

    Fixed build.sh

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ write_properties () {
   mkdir -p ./src/env
   
   if [ $1 = 'dev' ]; then
-    echo "VITE_GIT_REF=#$(git rev-parse --short HEAD)"
-    echo "VITE_GIT_REF=#$(git rev-parse --short HEAD)" > ./src/env/.env.local
+    echo "VITE_GIT_REF=$(git rev-parse --short HEAD)"
+    echo "VITE_GIT_REF=$(git rev-parse --short HEAD)" > ./src/env/.env.local
   elif [ $1 = 'test' ] || [ $1 = 'prod' ]; then
     echo "VITE_GIT_REF=$(git describe --tags)"
     echo "VITE_GIT_REF=$(git describe --tags)" > ./src/env/.env.local
