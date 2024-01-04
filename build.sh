365f906acbb369e9776a0924691e8ef64921b30c
Clinton Bush <clinton.bush@outlook.com>
2024-01-04 14:08:25 -0500

    Fixed build.sh

--- build.sh
+++ build.sh
@@ -4,8 +4,10 @@ write_properties () {
   mkdir -p ./src/env
   
   if [ $1 = 'dev' ]; then
+    echo "VITE_GIT_REF=#$(git rev-parse --short HEAD)"
     echo "VITE_GIT_REF=#$(git rev-parse --short HEAD)" > ./src/env/.env.local
   elif [ $1 = 'test' ] || [ $1 = 'prod' ]; then
+    echo "VITE_GIT_REF=$(git describe --tags)"
     echo "VITE_GIT_REF=$(git describe --tags)" > ./src/env/.env.local
   else
     echo "Argument 1 must be dev, test, or prod. $1 is not recognized."
