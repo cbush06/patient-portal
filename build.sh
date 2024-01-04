fe6c3089c4cf64603c0d66c1ffbfee03ce8c72ea
Clinton Bush <clinton.bush@outlook.com>
2024-01-04 10:47:33 -0500

    Fixed build.sh and gha

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 write_properties () {
   echo "Building for $1"
 
+  mkdir -p ./src/env
+
   if [ $1 = 'dev' ]; then
     echo "VITE_GIT_REF=$(git rev-parse --short HEAD)" > ./src/env/.env.local
   elif [ $1 = 'test' ] || [ $1 = 'prod' ]; then
