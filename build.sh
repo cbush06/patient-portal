8c163c9bc38db66161255b0bcc48c4e53e7f6ca7
Clinton Bush <clinton.bush@outlook.com>
2024-01-04 13:59:23 -0500

    Fixed build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 write_properties () {
   echo "Building for $1"
-
+  
   mkdir -p ./src/env
-
+  
   if [ $1 = 'dev' ]; then
-    echo "VITE_GIT_REF=$(git rev-parse --short HEAD)" > ./src/env/.env.local
+    echo "VITE_GIT_REF=#$(git rev-parse --short HEAD)" > ./src/env/.env.local
   elif [ $1 = 'test' ] || [ $1 = 'prod' ]; then
-    echo "VITE_GIT_REF=$(git describe)" > ./src/env/.env.local
+    echo "VITE_GIT_REF=$(git describe --tags)" > ./src/env/.env.local
   else
     echo "Argument 1 must be dev, test, or prod. $1 is not recognized."
     return 1
